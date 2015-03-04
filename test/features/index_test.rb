669082941024308fbd09fdeaf8e19166d506de16
Tyler Pottle <tyler.pottle@gmail.com>
2015-03-03 23:10:55 -0800

    add tests for static home page

--- test/features/index_test.rb
+++ test/features/index_test.rb
@@ -1,8 +1,25 @@
 require 'test_helper'
 
 describe 'my portfolio home page' do
-  it 'shows the welcome page' do
+  before do
     visit 'http://localhost:3000'
-    page.text.must_include 'Hello!'
+  end
+
+  it 'includes an About Me title' do
+    page.text.must_include 'About Me'
+  end
+
+  it 'includes an unordered list with class info' do
+    page.html.must_include "<ul class='info'>"
+  end
+
+  it 'includes an unordered list with contact info info' do
+    page.html.must_include "<ul class='contact info'>"
+  end
+
+  it 'includes a titles for contact links' do
+    page.text.must_include 'twitter'
+    page.text.must_include 'email'
+    page.text.must_include 'Github'
   end
 end
