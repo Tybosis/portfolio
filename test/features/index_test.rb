636d8da10285bdf1b58f95078098b49a557d6861
Tyler Pottle <tyler.pottle@gmail.com>
2015-03-09 12:55:48 -0700

    move article tests into articles subfolder

--- test/features/index_test.rb
+++ test/features/index_test.rb
@@ -2,24 +2,14 @@ require 'test_helper'
 
 describe 'my portfolio home page' do
   before do
-    visit 'http://localhost:3000'
+    visit 'http://www.tylerpottle.com'
   end
 
   it 'includes an About Me title' do
     page.text.must_include 'About Me'
   end
 
-  it 'includes an unordered list with class info' do
-    page.html.must_include "<ul class='info'>"
-  end
-
-  it 'includes an unordered list with contact info info' do
-    page.html.must_include "<ul class='contact info'>"
-  end
-
   it 'includes a titles for contact links' do
-    page.text.must_include 'twitter'
-    page.text.must_include 'email'
-    page.text.must_include 'Github'
+    page.html.must_include 'twitter'
   end
 end
