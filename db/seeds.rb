1e23a5744d71299e53bd81479318fc0d3f67debf
Tyler Pottle <tyler.pottle@gmail.com>
2015-03-14 17:20:18 -0700

    add fake data to seeds to visually test policies

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,22 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 editor = User.create!(role: 'editor', email: "email2@website.com", password: '1234fake')
+author = User.create!(role: 'author', email: "author@website.com", password: '1234fake')
 
-10.times do |x|
+5.times do |x|
 Article.create!(title: "#{Faker::Hacker.adjective} #{Faker::Hacker.noun}",
                 content: Faker::Hacker.say_something_smart, published: true,
                 author: editor)
+end
+
+5.times do |x|
+Article.create!(title: "#{Faker::Hacker.adjective} #{Faker::Hacker.noun}",
+                content: Faker::Hacker.say_something_smart, published: false,
+                author: editor)
+end
+
+5.times do |x|
+Article.create!(title: "#{Faker::Hacker.adjective} #{Faker::Hacker.noun}",
+                content: Faker::Hacker.say_something_smart, published: false,
+                author: author)
 end
