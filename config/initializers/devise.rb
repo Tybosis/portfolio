cd78c409fd4164a692ea7af34842a078401d47c7
Tyler Pottle <tyler.pottle@gmail.com>
2015-03-19 07:48:53 -0700

    add omniauth columns to users table

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,6 +5,7 @@ Devise.setup do |config|
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
   # config.secret_key = 'afd3f072dc10ff258e8882b3050206e6a964c7d25e9c62341837a2718869f9bbd46437edfdb9693108ae4ebfbdf7606a91e668804f5b234afbbf704c655d5967'
+config.omniauth :twitter, Rails.application.secrets.twitter_key, Rails.application.secrets.twitter_secret
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150316203216) do
+ActiveRecord::Schema.define(version: 20150319143957) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -61,6 +61,9 @@ ActiveRecord::Schema.define(version: 20150316203216) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "role"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
